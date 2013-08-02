b3de7c62a628ff08439c0f8467a1e2256043cd49
Joy Xi <joy.b.xi@gmail.com>
2013-08-01 21:51:20 -0500

    Changed ChessPieces to include subclasses by piece type, added input for moves

--- Chess Summer Final Proj/ChessBoard.java
+++ Chess Summer Final Proj/ChessBoard.java
@@ -1,4 +1,4 @@
-import java.lang.*;
+import java.io.*;
 import java.util.*;
 
 /* Chess Board
@@ -17,18 +17,17 @@ class ChessBoard {
 	ChessPiece board[][];
 	HashMap<String, Integer> letterMap = new HashMap<String, Integer>();
 		
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 		ChessBoard activeBoard = new ChessBoard();
 		activeBoard.display();
 		activeBoard.play();
 	}
 	
-	
 	ChessBoard() {
 		activePlayer = WHITE;
 		char ch;
 		String s;
-		// Hashmap to store column names on chess board
+		// HashMap to store column names on chess board
 		for (int i = 1; i <=8; i++) {
 			ch = (char) ((int) 'a'+i-1);
 			s = String.valueOf(ch);
@@ -36,40 +35,44 @@ class ChessBoard {
 		}
 		
 		board = new ChessPiece[9][9];
-		board[1][letterMap.get("a").intValue()] = new ChessPiece('R'); // rook
-		board[1][letterMap.get("b")] = new ChessPiece('K'); // knight
-		board[1][letterMap.get("c")] = new ChessPiece('B'); // bishop
-		board[1][letterMap.get("d")] = new ChessPiece('K'); // king
-		board[1][letterMap.get("e")] = new ChessPiece('Q'); // queen
-		board[1][letterMap.get("f")] = new ChessPiece('B'); // bishop
-		board[1][letterMap.get("g")] = new ChessPiece('K'); // knight
-		board[1][letterMap.get("h")] = new ChessPiece('R'); // rook
+		board[1][letterMap.get("a").intValue()] = new Rook();
+		board[1][letterMap.get("b")] = new Knight();
+		board[1][letterMap.get("c")] = new Bishop();
+		board[1][letterMap.get("d")] = new King();
+		board[1][letterMap.get("e")] = new Queen();
+		board[1][letterMap.get("f")] = new Bishop();
+		board[1][letterMap.get("g")] = new Knight();
+		board[1][letterMap.get("h")] = new Rook();
 		for (int r = 2; r < 8; r++) {
 			for (int c = 1; c<= 8; c++) {
 				if (r == 2 || r == 7)
-					board[r][c] = new ChessPiece('P');
+					board[r][c] = new Pawn();
 				else
 					board[r][c] = new ChessPiece();
 			}
 		}
-		board[8][letterMap.get("a")] = new ChessPiece('R'); // rook
-		board[8][letterMap.get("b")] = new ChessPiece('K'); // knight
-		board[8][letterMap.get("c")] = new ChessPiece('B'); // bishop
-		board[8][letterMap.get("d")] = new ChessPiece('K'); // king
-		board[8][letterMap.get("e")] = new ChessPiece('Q'); // queen
-		board[8][letterMap.get("f")] = new ChessPiece('B'); // bishop
-		board[8][letterMap.get("g")] = new ChessPiece('K'); // knight
-		board[8][letterMap.get("h")] = new ChessPiece('R'); // rook	}
+		board[8][letterMap.get("a").intValue()] = new Rook();
+		board[8][letterMap.get("b")] = new Knight();
+		board[8][letterMap.get("c")] = new Bishop();
+		board[8][letterMap.get("d")] = new King();
+		board[8][letterMap.get("e")] = new Queen();
+		board[8][letterMap.get("f")] = new Bishop();
+		board[8][letterMap.get("g")] = new Knight();
+		board[8][letterMap.get("h")] = new Rook();
 	}
 	
-	void play() {
+	void play() throws IOException {
 		// TEMP repeat while game is still going on, until one of players enters "QUIT"
 		// get move input
 		// if move is valid...
 		// perform move
-		if (makeMove("A1","A5"))
-			System.out.println();
-			
+		BufferedReader stream = new BufferedReader(new InputStreamReader(System.in));
+		String s1 = stream.readLine();
+		String s2 = stream.readLine();
+		if (makeMove(s1,s2))
+			System.out.println("Move successful");
+		else
+			System.out.println("Attempted bad move");
 		display();
 		// display board
 		// if check or checkmate, say so
@@ -108,14 +111,14 @@ class ChessBoard {
 		int toR = Integer.parseInt(t.substring(1, t.length()));
 		int toC = letterMap.get(t.toLowerCase().substring(0,1));
 		// TEMP fill in more logic for moving
+		System.out.println("Move from ("+fromR+", "+fromC+") to ("+toR+", "+toC+").");
 		if (board[fromR][fromC].validMove(fromR, fromC, toR, toC)) {
 			ChessPiece temp = board[toR][toC];
 			board[toR][toC] = board[fromR][fromC];
 			board[fromR][fromC] = temp;
 			
 			/* output for testing
- 			System.out.println("Move from ("+fromR+", "+fromC+") to ("+toR+", "+toC+").");
-			System.out.println(temp);
+ 			System.out.println(temp);
 			System.out.println(board[fromR][fromC]);
 			System.out.println(board[toR][toC]);
 			*/
@@ -125,40 +128,112 @@ class ChessBoard {
 	}
 	
 	class ChessPiece {
-		
-		boolean empty;
 		char pieceName;
 		
-		// constructor
-		ChessPiece (char n) {
-			pieceName = n;
-			empty = false;
-			// TEMP - will override in each subclass
-		}
-		
 		ChessPiece () {
-			empty = true;
-			// TEMP - will override in each subclass
+			// if empty, set pieceName to be null
+			pieceName = ' ';
 		}
 		
 		// toString for printing board
 		public String toString() {
-			if(empty)
+			if(isEmpty())
 				return "";
 			else
 				return Character.toString(pieceName);
 		}
 		
 		boolean isEmpty() {
-			return empty;
+			return (pieceName == ' ');
 		}
+		
 		/* method: validMove(from, to)
 		 * returns whether something is a valid move for this particular piece
 		 */
 		 boolean validMove (int fr, int fc, int tr, int tc) {
-			return true;
-			// TEMP fill with logic for whether something is a valid move
+			return false;
 		}
 	}
+	
+	class King extends ChessPiece {
+		King() {
+			pieceName = 'K';
+		}
+		// king can move exactly one vacant square in any direction
+		boolean validMove (int fr, int fc, int tr, int tc) {
+			if ((Math.abs(fr - tr) == 1) && (Math.abs(fc - tc) == 0))
+				return true;
+			else if ((Math.abs(fr - tr) == 0) && (Math.abs(fc - tc) == 1))
+				return true;
+			else
+				return false;
+			// TEMP add possibility of castling
+		}
+	}
+	class Queen extends ChessPiece {
+		Queen() {
+			pieceName = 'Q';
+		}
+		boolean validMove (int fr, int fc, int tr, int tc) {
+			// can go in diagonals
+			if (fr - tr == fc - tc)
+				return true;
+			// can also go in straight lines
+			else if ((fr == tr) || (fc == tc))
+				return true;
+			else
+				return false;
+		}
+	}
+	class Knight extends ChessPiece {
+		Knight() {
+			pieceName = 'N';
+		}
+		boolean validMove (int fr, int fc, int tr, int tc) {
+			if ((Math.abs(fr - tr) == 2) && (Math.abs(fc - tc) == 1))
+				return true;
+			else if ((Math.abs(fr - tr) == 1) && (Math.abs(fc - tc) == 2))
+				return true;
+			else
+				return false;
+		}
+	}
+	class Rook extends ChessPiece {
+		Rook() {
+			pieceName = 'R';
+		}
+		boolean validMove (int fr, int fc, int tr, int tc) {
+			if ((fr == tr) || (fc == tc))
+				return true;
+			else
+				return false;
+		}
+	}
+	class Bishop extends ChessPiece {
+		Bishop() {
+			pieceName = 'B';
+		}
+		boolean validMove (int fr, int fc, int tr, int tc) {
+			if (fr - tr == fc - tc)
+				return true;
+			else
+				return false;
+		}
+	}
+	class Pawn extends ChessPiece {
+		boolean hasMoved;
+		Pawn() {
+			pieceName = 'P';
+			hasMoved = false;
+		}
+		boolean validMove (int fr, int fc, int tr, int tc) {
+			if ((tr - fr == 1) && (fc == tc))
+				return true;
+			else
+				return false;
+			// TEMP need to account for ability to move two in first move
+		}
+	}
+
 }
 
