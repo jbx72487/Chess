68994321da95b750c705b82be3e848d60814e379
Joy Xi <joy.b.xi@gmail.com>
2013-08-02 15:30:31 -0500

    Added ChessCoord class

--- Chess Summer Final Proj/ChessBoard.java
+++ Chess Summer Final Proj/ChessBoard.java
@@ -10,15 +10,16 @@ class ChessBoard {
 	// activePlayer shows who is playing
 	int activePlayer;
 	// constants WHITE and BLACK represent two players
-	static int WHITE = 1;
-	static int BLACK = -1;
+	static final int WHITE = 1;
+	static final int BLACK = -1;
 	
 	// board holds pointers to all the ChessPiece objects
 	ChessPiece board[][];
 	HashMap<String, Integer> letterMap = new HashMap<String, Integer>();
 	
 	// coordinates of two kings
-	int whiteKingR, whiteKingC, blackKingR, blackKingC;
+	ChessCoord whiteKing;
+	ChessCoord blackKing;
 		
 	public static void main(String args[]) throws IOException {
 		new ChessBoard();
@@ -42,8 +43,7 @@ class ChessBoard {
 		board[1][letterMap.get("b")] = new Knight(WHITE);
 		board[1][letterMap.get("c")] = new Bishop(WHITE);
 		board[1][letterMap.get("d")] = new King(WHITE);
-		whiteKingR = 1;
-		whiteKingC = letterMap.get("d");
+		whiteKing = new ChessCoord(1,letterMap.get("d"));
 		board[1][letterMap.get("e")] = new Queen(WHITE);
 		board[1][letterMap.get("f")] = new Bishop(WHITE);
 		board[1][letterMap.get("g")] = new Knight(WHITE);
@@ -62,8 +62,7 @@ class ChessBoard {
 		board[8][letterMap.get("b")] = new Knight(BLACK);
 		board[8][letterMap.get("c")] = new Bishop(BLACK);
 		board[8][letterMap.get("d")] = new King(BLACK);
-		whiteKingR = 8;
-		whiteKingC = letterMap.get("d");
+		blackKing = new ChessCoord(8,letterMap.get("d"));
 		board[8][letterMap.get("e")] = new Queen(BLACK);
 		board[8][letterMap.get("f")] = new Bishop(BLACK);
 		board[8][letterMap.get("g")] = new Knight(BLACK);
@@ -71,6 +70,10 @@ class ChessBoard {
 		
 		// play chess!
 		play();
+	}
+	
+	ChessPiece getPiece(ChessCoord a) {
+		return board[a.row][a.col];
 	}
 	
 	void play() throws IOException {
@@ -111,7 +114,6 @@ class ChessBoard {
 		for (int c = (int)('A'); c <= (int) ('H'); c++)
 			System.out.print("  "+ (char) c +" ");
 		System.out.println();
-		System.out.println();
 		// actual board
 		for (int r = 8; r >= 1; r--) {
 			System.out.print(r + " ");
@@ -128,7 +130,6 @@ class ChessBoard {
 		}
 		// bottom row labels
 		System.out.print("  ");
-		System.out.println();
 		for (int c = (int)('A'); c <= (int) ('H'); c++)
 			System.out.print("  "+ (char) c +" ");
 		System.out.println();
@@ -136,8 +137,15 @@ class ChessBoard {
 	
 	// hasCheck checks whether current player has a check
 	boolean hasCheck() {
-		// pawn can eat from diagonally in front
-		// 
+		// look for pawn 1 space diagonally in front
+		
+		// look for rook horiz or vert in any direction
+		// look for bishop diag in any direction
+		// look for queen horiz, vert, or diag in any direction
+		// look for king 1 space in any direction
+		// look for knight an "L" away
+		
+		
 		
 		return false;
 		// TEMP fill with logic for checking for check
@@ -159,50 +167,49 @@ class ChessBoard {
 			toR = Integer.parseInt(m.substring(4, 5));
 			toC = letterMap.get(m.substring(3,4));
 		}
-		catch (Exception e) {System.out.println("parse error"); return false;}
+		catch (Exception e) {return false;}
+		
+		ChessCoord from = new ChessCoord(fromR, fromC);
+		ChessCoord to = new ChessCoord(toR, toC);
+		
+		
 		// TEMP this output is for testing only
 		System.out.println("Move from ("+fromR+", "+fromC+") to ("+toR+", "+toC+").");
-		if (onBoard(fromR, fromC) && onBoard(toR, toC) && // both "from" and "to" spaces must be on the gameboard
-				board[fromR][fromC].getColor() == activePlayer &&// can only move your own piece
-				!board[fromR][fromC].isEmpty() && // "from" space must be occupied
-				!(fromR == toR && fromC == toC) && // "from" and "to" can't be the same space
-				board[fromR][fromC].validMove(fromR, fromC, toR, toC)) { // must be valid move based on that gamepiece
+		if (from.isValid() && to.isValid() && // both "from" and "to" spaces must be on the gameboard
+				getPiece(from).getColor() == activePlayer &&// can only move your own piece
+				!getPiece(from).isEmpty() && // "from" space must be occupied
+				!(from.isEqual(to)) && // "from" and "to" can't be the same space
+				board[fromR][fromC].validMove(from, to)) { // must be valid move based on that gamepiece
 			// if "to" space is empty, can just switch the "from" and "to"
-			if (board[toR][toC].isEmpty()) {
-				ChessPiece temp = board[toR][toC];
-				board[toR][toC] = board[fromR][fromC];
+			if (getPiece(to).isEmpty()) {
+				ChessPiece temp = getPiece(to);
+				board[toR][toC] = getPiece(from);
 				board[fromR][fromC] = temp;
 				// if pawn that hasn't moved before, mark it as moved
-				if (board[toR][toC].getName() == 'P') {
-					Pawn p = (Pawn) board[toR][toC];
+				if (getPiece(to).getName() == 'P') {
+					Pawn p = (Pawn) getPiece(to);
 					if (!p.hasMoved)
 						p.move();
 				}
-				if (board[toR][toC].getName() == 'K') {
+				if (getPiece(to).getName() == 'K') {
 					if (activePlayer == WHITE) {
-						whiteKingR = toR;
-						whiteKingC = toC;
+						// TEMP - figure out how to do this more elegantly in one assignment
+						whiteKing.row = toR;
+						whiteKing.col = toC;
 					} else {
-						blackKingR = toR;
-						blackKingC = toC;
+						blackKing.row = toR;
+						blackKing.col = toC;
 					}
 				}
 			} else { // if the "to" isn't empty
 				// if it's on your own side, can't make this move
 				if (activePlayer == board[toR][toC].getColor()) return false; // "to" space must be on other side
 				System.out.println(board[toR][toC]+" captured.");
-				board[toR][toC].remove();
-				ChessPiece temp = board[toR][toC];
-				board[toR][toC] = board[fromR][fromC];
+				getPiece(to).remove();
+				ChessPiece temp = getPiece(to);
+				board[toR][toC] = getPiece(from);
 				board[fromR][fromC] = temp;
 			}
-			// TEMP still need to test if everything in middle is empty
-			
-			/* output for testing
- 			System.out.println(temp);
-			System.out.println(board[fromR][fromC]);
-			System.out.println(board[toR][toC]);
-			*/
 			
 			// Promotion
 			// If the pawn reaches a square on the back rank of the opponent, it promotes to the player's choice of a queen, rook, bishop, or knight
@@ -231,10 +238,25 @@ class ChessBoard {
 		} else return false;
 	}
 	
-	// checks to make sure a point is on board
-	boolean onBoard(int r, int c) {
-		return (r >= 1 && r <= 8 &&
-				c >= 1 && c <= 8);
+	
+	class ChessCoord {
+		int row;
+		int col;
+		
+		ChessCoord(int r, int c) {
+			row = r;
+			col = c;
+		}
+		
+		boolean isValid() {
+			// checks to make sure a point is on board
+			return (row >= 1 && row <= 8 &&
+					col >= 1 && col <= 8);
+		}
+		
+		boolean isEqual(ChessCoord a) {
+			return (row == a.row && col == a.col);
+		}
 	}
 	
 	class ChessPiece {
@@ -258,10 +280,9 @@ class ChessBoard {
 			return (pieceName == ' ');
 		}
 		
-		/* method: validMove(from, to)
-		 * returns whether something is a valid move for this particular piece
-		 */
-		 boolean validMove (int fr, int fc, int tr, int tc) {
+		 		 
+		 boolean validMove (ChessCoord f, ChessCoord t) {
+			// returns whether something is a valid move for this particular piece
 			return false;
 		}
 		 
@@ -284,9 +305,9 @@ class ChessBoard {
 			color = c;
 		}
 		// king can move exactly one vacant square in any direction
-		boolean validMove (int fr, int fc, int tr, int tc) {
-			if (((Math.abs(fr - tr) == 1) && (Math.abs(fc - tc) == 0)) || ((Math.abs(fr - tr) == 0) && (Math.abs(fc - tc) == 1)) ||
-					(Math.abs(fr - tr) == 1 && Math.abs(fc - tc) == 1))
+		boolean validMove (ChessCoord f, ChessCoord t) {
+			if (((Math.abs(f.row - t.row) == 1) && (Math.abs(f.col - t.col) == 0)) || ((Math.abs(f.row - t.row) == 0) && (Math.abs(f.col - t.col) == 1)) ||
+					(Math.abs(f.row - t.row) == 1 && Math.abs(f.col - t.col) == 1))
 				return true;
 			else
 				return false;
@@ -298,33 +319,33 @@ class ChessBoard {
 			pieceName = 'Q';
 			color = c;
 		}
-		boolean validMove (int fr, int fc, int tr, int tc) {
+		boolean validMove (ChessCoord f, ChessCoord t) {
 			// can go in diagonals or straight lines
-			if ((fc - tc)/(fr - tr) == 1) {
+			if ((t.col - f.col)/(t.row - f.row) == 1) {
 				// if positive slope
-				int c = Math.min(fc, tc) + 1;
-				for (int r = Math.min(fr,tr) + 1; r < Math.max(fr, tr); r++) {
+				int c = Math.min(f.col, t.col) + 1;
+				for (int r = Math.min(f.row,t.row) + 1; r < Math.max(f.row, t.row); r++) {
 					if (!board[r][c].isEmpty()) return false;
 					c++;
 				}
 				return true;
-			} else if ((fc - tc)/(fr - tr) == -1) {
+			} else if ((t.col - f.col)/(t.row - f.row) == -1) {
 				// if negative slope
-				int c = Math.min(fc, tc) + 1;
-				for (int r = Math.max(fr,tr) - 1; r > Math.min(fr, tr); r--) {
+				int c = Math.min(f.col, t.col) + 1;
+				for (int r = Math.max(f.row,t.row) - 1; r > Math.min(f.row, t.row); r--) {
 					if (!board[r][c].isEmpty()) return false;
 					c++;
 				}
 				return true;
-			} else if (fr == tr) {
+			} else if (t.row == f.row) {
 				// if going horiz, check for pieces in between
-				for (int c = Math.min(fc, tc)+1; c < Math.max(fc, tc); c++)
-					if (!board[fr][c].isEmpty()) return false;
+				for (int c = Math.min(f.col, t.col)+1; c < Math.max(f.col, t.col); c++)
+					if (!board[f.row][c].isEmpty()) return false;
 				return true;
-			} else if (fc == tc) {
+			} else if (t.col == f.col) {
 				// if going vert, check for pieces in between
-				for (int r = Math.min(fr, tr)+1; r < Math.max(fr, tr); r++)
-					if (!board[r][fc].isEmpty()) return false;
+				for (int r = Math.min(f.row, t.row)+1; r < Math.max(f.row, t.row); r++)
+					if (!board[r][f.col].isEmpty()) return false;
 				return true;
 			} else
 				return false;
@@ -335,9 +356,9 @@ class ChessBoard {
 			pieceName = 'N';
 			color = c;
 		}
-		boolean validMove (int fr, int fc, int tr, int tc) {
-			if (((Math.abs(fr - tr) == 2) && (Math.abs(fc - tc) == 1)) ||
-					((Math.abs(fr - tr) == 1) && (Math.abs(fc - tc) == 2)))
+		boolean validMove (ChessCoord f, ChessCoord t) {
+			if (((Math.abs(t.row - f.row) == 2) && (Math.abs(t.col - f.col) == 1)) ||
+					((Math.abs(t.row - f.row) == 1) && (Math.abs(t.col - f.col) == 2)))
 				return true;
 			else
 				return false;
@@ -348,16 +369,16 @@ class ChessBoard {
 			pieceName = 'R';
 			color = c;
 		}
-		boolean validMove (int fr, int fc, int tr, int tc) {
-			if (fr == tr) {
+		boolean validMove (ChessCoord f, ChessCoord t) {
+			if (t.row == t.row) {
 				// if going horiz, check for pieces in between
-				for (int c = Math.min(fc, tc)+1; c < Math.max(fc, tc); c++)
-					if (!board[fr][c].isEmpty()) return false;
+				for (int c = Math.min(f.col, t.col)+1; c < Math.max(f.col, t.col); c++)
+					if (!board[f.row][c].isEmpty()) return false;
 				return true;
-			} else if (fc == tc) {
+			} else if (t.col == f.col) {
 				// if going vert, check for pieces in between
-				for (int r = Math.min(fr, tr)+1; r < Math.max(fr, tr); r++)
-					if (!board[r][fc].isEmpty()) return false;
+				for (int r = Math.min(f.row, t.row)+1; r < Math.max(f.row, t.row); r++)
+					if (!board[r][f.col].isEmpty()) return false;
 				return true;
 			} else
 				return false;
@@ -368,19 +389,19 @@ class ChessBoard {
 			pieceName = 'B';
 			color = c;
 		}
-		boolean validMove (int fr, int fc, int tr, int tc) {
-			if ((fc - tc)/(fr - tr) == 1) {
+		boolean validMove (ChessCoord f, ChessCoord t) {
+			if ((f.col - t.col)/(f.row - t.row) == 1) {
 				// if positive slope
-				int c = Math.min(fc, tc) + 1;
-				for (int r = Math.min(fr,tr) + 1; r < Math.max(fr, tr); r++) {
+				int c = Math.min(f.col, t.col) + 1;
+				for (int r = Math.min(f.row,t.row) + 1; r < Math.max(f.row, t.row); r++) {
 					if (!board[r][c].isEmpty()) return false;
 					c++;
 				}
 				return true;
-			} else if ((fc - tc)/(fr - tr) == -1) {
+			} else if ((f.col - t.col)/(f.row - t.row) == -1) {
 				// if negative slope
-				int c = Math.min(fc, tc) + 1;
-				for (int r = Math.max(fr,tr) - 1; r > Math.min(fr, tr); r--) {
+				int c = Math.min(f.col, t.col) + 1;
+				for (int r = Math.max(f.row,t.row) - 1; r > Math.min(f.row, t.row); r--) {
 					if (!board[r][c].isEmpty()) return false;
 					c++;
 				}
@@ -399,15 +420,15 @@ class ChessBoard {
 		void move() {
 			hasMoved = true;
 		}
-		boolean validMove (int fr, int fc, int tr, int tc) {
+		boolean validMove (ChessCoord f, ChessCoord t) {
 			// if moving forward 1 in the same column and the target is empty, return true
-			if ((tr - fr == color) && (fc == tc) && board[tr][tc].isEmpty())
+			if ((t.row - f.row == color) && (f.col == t.col) && getPiece(t).isEmpty())
 				return true;
 			// if moving forward 1 and sideways 1 and the target is not empty, return true
-			else if ((tr - fr == color) && (Math.abs(tc-fc) == 1) && !board[tr][tc].isEmpty())
+			else if ((t.row - f.row == color) && (Math.abs(t.col-f.col) == 1) && !getPiece(t).isEmpty())
 				return true;
 			// if it's this pawn's fist move and moving forward 2 in the same column and the target is empty, return true
-			else if (!hasMoved && (tr - fr == color*2) && (fc == tc) && board[tr][tc].isEmpty())
+			else if (!hasMoved && (t.row - f.row == color*2) && (f.col == t.col) && getPiece(t).isEmpty())
 				return true;
 			else
 				return false;
